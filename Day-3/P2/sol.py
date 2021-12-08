ee5912251077e51d81bbbb9f5858a0f3a4d023ef
David Bergh <berghdavid@hotmail.com>
2021-12-08 23:10:48 +0100

    Update Readme

--- Day-3/P2/sol.py
+++ Day-3/P2/sol.py
@@ -1,33 +1,42 @@
-def part2():
-  ns = []
+matrix = list()
+current_ox = list()
+current_co = list()
+
+def parse_input():
+  global matrix, current_ox, current_co
   with (open("i")) as f:
-    ns = f.readlines()
-  current_ox = ns.copy()
-  current_co = ns.copy()
+    matrix = f.readlines()
+  
+  current_ox = matrix.copy()
+  current_co = matrix.copy()
 
-  def do_alg(l, bit, f):
-    if len(l) == 1:
-      return l
-    zeros = []
-    ones = []
-    for n in l:
-      if n[bit] == "0":
-        zeros.append(n)
-      else:
-        ones.append(n)
+def alg(l, bit, f):
+  if len(l) == 1:
+    return l
+  zeros = []
+  ones = []
+  for n in l:
+    if n[bit] == "0":
+      zeros.append(n)
+    else:
+      ones.append(n)
 
-    return f(zeros, ones)
+  return f(zeros, ones)
 
-  for i in range(len(ns[0])):
-    current_ox = do_alg(
+def solve():
+  global matrix, current_ox, current_co
+  
+  for i in range(len(matrix[0])):
+    current_ox = alg(
       current_ox, i, lambda zeros, ones: zeros if len(zeros) > len(ones) else ones
     )
-    current_co = do_alg(
+    current_co = alg(
       current_co, i, lambda zeros, ones: ones if len(zeros) > len(ones) else zeros
     )
-
   return int(f"0b{current_ox[0]}", 2) * int(f"0b{current_co[0]}", 2)
 
+def main():
+  parse_input()
+  print(solve())
 
-if __name__ == "__main__":
-    print(part2())
+main()
