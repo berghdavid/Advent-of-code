79c736033e9e7f38e2a46cde81992eb344bcb4cf
David Bergh <berghdavid@hotmail.com>
2022-12-01 09:25:11 +0100

    Init 02

---
+++ 2022/02/task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+        int     ans;
+
+        printf("Task 1 ans: %d\n", ans);
+
+        return 0;
+}
---
+++ 2022/02/task2.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include <stdio.h>
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+        int     ans;
+        
+        printf("Task 2 ans: %d\n", ans);
+
+        return 0;
+}
