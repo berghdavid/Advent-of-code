46ef92874d8cbaae9257ea7dbae77af8da531af3
David Bergh <berghdavid@hotmail.com>
2022-12-02 13:23:55 +0100

    02-A1 done

--- 2022/02/task1.c
+++ 2022/02/task1.c
@@ -1,11 +1,111 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "utils.h"
+
+typedef struct Game Game;
+
+struct Game {
+	char	elf;
+	char	me;
+	Game*	next;
+};
+
+Game* build_games()
+{
+	Game*	first;
+	Game*	g;
+	char	c;
+
+	/* Init first element */
+	first = malloc(sizeof(Game));
+	first->elf = getchar();
+	getchar();
+	first->me = getchar();
+	getchar();
+	first->next = NULL;
+
+	g = first;
+	
+	while((c = getchar()) != EOF) {
+		g->next = malloc(sizeof(Game));
+		g = g->next;
+		g->elf = c;
+		g->next = NULL;
+		getchar();
+		g->me = getchar();
+		getchar();
+	}
+
+	return first;
+}
+
+void free_games(Game* first)
+{
+	Game*	i;
+	Game*	temp;
+
+	i = first;
+
+	while (i != NULL) {
+		temp = i->next;
+		free(i);
+		i = temp;
+	}
+}
+
+int pts(char elf, char me)
+{
+	int	score;
+	int	pos;
+
+	score = 0;
+
+	if (me == 'X') {
+		score += 1;
+	} else if (me == 'Y') {
+		score += 2;
+	} else {
+		score += 3;
+	}
+	pos = score;
+
+	if (elf == 'B') {
+		pos += 2;
+	} else if (elf == 'C') {
+		pos += 1;
+	}
+	pos = pos % 3;
+
+	return score + (pos * 3);
+}
+
+int sum_points(Game* g)
+{
+	Game*	i;
+	int	sum;
+
+	sum = 0;
+	i = g;
+
+	while (i != NULL) {
+		sum += pts(i->elf, i->me);
+		i = i->next;
+	}
+
+	return sum;
+}
 
 int main(int argc, char *argv[])
 {
-        int     ans;
+	int     ans;
+	Game*	g;
 
-        printf("Task 1 ans: %d\n", ans);
+	g = build_games();
 
-        return 0;
+	ans = sum_points(g);
+
+	printf("Task 1 ans: %d\n", ans);
+
+	free_games(g);
+	return 0;
 }
