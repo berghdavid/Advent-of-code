ea4bd39e81352168ec1a671f06f43336fc5f280a
David Bergh <berghdavid@hotmail.com>
2022-12-03 19:29:02 +0100

    Init 04

---
+++ 2022/04/task1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+	int	ans;
+
+	ans = 0;
+
+	printf("Task 1 ans: %d\n", ans);
+
+	return 0;
+}
---
+++ 2022/04/task2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+	int	ans;
+
+	ans = 0;
+
+	printf("Task 2 ans: %d\n", ans);
+
+	return 0;
+}
