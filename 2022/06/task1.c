f7ca62d8e73bfd0aaa6bb58719afa690150c16e2
David Bergh <berghdavid@hotmail.com>
2022-12-05 19:10:28 +0100

    Init 06

---
+++ 2022/06/task1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "../common/utils.h"
+
+int main(void)
+{
+	int	ans;
+
+	ans = 0;
+
+	printf("Task 1 ans: %d\n", ans);
+
+	return 0;
+}
---
+++ 2022/06/task2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "../common/utils.h"
+
+int main(void)
+{
+	int	ans;
+
+	ans = 0;
+
+	printf("Task 2 ans: %d\n", ans);
+
+	return 0;
+}
