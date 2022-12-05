afcae48e7afce38d0038f011ea5dc5bf978dbfbf
David Bergh <berghdavid@hotmail.com>
2022-12-05 08:43:44 +0100

    Init 05

---
+++ 2022/05/task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int solve()
+{
+	int	ans;
+
+	ans = 0;
+
+	return ans;
+}
+
+int main(int argc, char *argv[])
+{
+	int	ans;
+
+	ans = solve();
+
+	printf("Task 2 ans: %d\n", ans);
+
+	return 0;
+}
---
+++ 2022/05/utils.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "utils.h"
+
+int next_int()
+{
+        int     x;
+        int     c;
+
+	x = 0;
+
+        if((c = getchar()) == EOF) {
+                return -1;
+        }
+        while (isdigit(c)) {
+                x = 10 * x + c - '0';
+                c = getchar();
+        }
+
+        return x;
+}
