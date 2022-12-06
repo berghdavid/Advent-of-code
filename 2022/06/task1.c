479f71bbdc69f8d39b634d37f07f41a23c00a14d
David Bergh <berghdavid@hotmail.com>
2022-12-06 10:09:43 +0100

    06-01 done

--- 2022/06/task1.c
+++ 2022/06/task1.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include "../common/utils.h"
+
+int count_marker_length()
+{
+	int	length;
+	char	c;
+	char	c1;
+	char	c2;
+	char	c3;
+
+	c3 = getchar();
+	c2 = getchar();
+	c1 = getchar();
+	length = 4;
+	while ((c = getchar()) != EOF) {
+		if (c != c1 && c != c2 && c != c3 && c1 != c2 && c1 != c3 && c2 != c3) {
+			return length;
+		}
+		c3 = c2;
+		c2 = c1;
+		c1 = c;
+		length++;
+	}
+
+	return length;
+}
 
 int main(void)
 {
 	int	ans;
 
-	ans = 0;
+	ans = count_marker_length();
 
 	printf("Task 1 ans: %d\n", ans);
 
