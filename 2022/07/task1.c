6997ffcab407459dd4466b13f34020dd76b72037
David Bergh <berghdavid@hotmail.com>
2022-12-08 17:14:50 +0100

    wip

--- 2022/07/task1.c
+++ 2022/07/task1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+
+const int MAX_STRING = 11;
 
 typedef struct Dir Dir;
 
@@ -11,6 +14,16 @@ struct Dir {
 	Dir*	par;
 	Dir*	sub;
 };
+
+void finish_row()
+{
+	char	c;
+
+	c = getchar();
+	while (c != '\n') {
+		c = getchar();
+	}
+}
 
 void print_subdirs(Dir* d, int depth)
 {
@@ -51,22 +64,28 @@ Dir* malloc_dir(char* name, Dir* parent, int subfolders, int size)
 	return d;
 }
 
-void init_dir(Dir* d, char* name, int subfolders, int size)
+void init_subdirs(Dir* d, char** names, int subfolders)
 {
 	int	i;
 
-	d->name = name;
-	d->size = size;
 	d->subs = subfolders;
 	if (subfolders == 0) {
 		d->sub = NULL;
 	} else {
 		d->sub = malloc(subfolders * sizeof(Dir));
 		for (i = 0; i < subfolders; i++) {
-			d->sub[i].par = d;
-			init_dir(&d->sub[i], NULL, 0, 0);
+			init_empty_dir(&d->sub[i], d, names[i]);
 		}
 	}
+}
+
+void init_empty_dir(Dir* d, Dir* parent, char* name)
+{
+	d->name = name;
+	d->size = 0;
+	d->subs = 0;
+	d->sub = NULL;
+	d->par = parent;
 }
 
 void clean_subdir(Dir* d)
@@ -86,9 +105,86 @@ void clean_subdir(Dir* d)
 	free(d->sub);
 }
 
+char* read_string()
+{
+	char*	str;
+	char	c;
+	int	i;
+
+	str = malloc(MAX_STRING * sizeof(char));
+	str[MAX_STRING - 1] = '\0';
+
+	i = 0;
+	while ((c = getchar()) != '\n') {
+		str[i++] = c;
+	}
+
+	return str;
+}
+
+Dir* find_subdir(Dir* curr, char* name)
+{
+	int	i;
+
+	for (i = 0; i < curr->subs; i++) {
+		if (strcmp(curr->sub[i].name, name) == 0) {
+			return &curr->sub[i];
+		}
+	}
+	return curr;
+}
+
 void read_to_dir(Dir* d)
 {
+	int	nbr;
+	int	subs;
+	char	c;
+	char**	strings;
+	Dir*	curr;
 
+	strings = malloc(10 * sizeof(char*));
+	curr = d;
+	subs = 0;
+	nbr = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (isdigit(c)) {
+			nbr = 10 * nbr + c - '0';
+		} else if (nbr > 0) {
+			printf("%s: %d -> %d = %d\n",
+				curr->name, nbr, curr->size, nbr + curr->size);
+			curr->size += nbr;
+			nbr = 0;
+			finish_row();
+		} else if (c == 'd') {
+			/* Current row is "dir xxxxx" */
+			getchar();
+			getchar();
+			getchar();
+			getchar();
+			strings[subs++] = read_string();
+			subs++;
+		} else if (c == '$') {
+			/* There will always be a "$ cd " here */
+			init_subdirs(curr, strings, subs);
+			subs = 0;
+
+			getchar();
+			getchar();
+			getchar();
+
+			c = getchar();
+			if (c == '.') {
+				curr = curr->par;
+				finish_row();
+			} else {
+				strings[0] = read_string();
+				curr = find_subdir(curr, strings[0]);
+				free(strings[0]);
+			}
+		}
+	}
+	free(strings);
 }
 
 Dir* build_system()
@@ -101,8 +197,13 @@ Dir* build_system()
 	name[1] = '\0';
 
 	root = malloc(sizeof(Dir));
+	root->name = name;
+	root->par = NULL;
+	root->size = 0;
 
-	init_dir(root, name, 3, 100);
+	finish_row();
+	finish_row();
+	read_to_dir(root);
 
 	return root;
 }
@@ -115,16 +216,7 @@ void solve(char* ans)
 	size = 0;
 	root = build_system();
 
-	/*
-	while ((c = getchar()) != EOF) {
-		if (isdigit(c)) {
-			nbr = 10 * nbr + c - '0';
-		} else if (nbr > 0) {
-			//printf("Adding: %d -> %d = %d\n", nbr, size, nbr+size);
-			size += nbr;
-			nbr = 0;
-		}
-	}*/
+	/* TODO: Read system */
 
 	sprintf(ans, "%d", size);
 
