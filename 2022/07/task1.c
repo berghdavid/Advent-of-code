febcac3cfc0abbea1aed4f910c831972618aa94e
David Bergh <berghdavid@hotmail.com>
2022-12-08 09:01:35 +0100

    WIP

--- 2022/07/task1.c
+++ 2022/07/task1.c
@@ -1,10 +1,56 @@
 #include <stdio.h>
+#include <ctype.h>
+
+int get_file_size()
+{
+        int     x;
+        int     c;
+
+	x = 0;
+
+        if((c = getchar()) == '$') {
+                return -2;
+        } else if() {
+		
+	}
+        while (isdigit(c)) {
+                x = 10 * x + c - '0';
+                c = getchar();
+        }
+
+        return x;
+}
+
+int next_ls()
+{
+	char	c;
+	int	size;
+
+	while ((c = getchar()) != EOF) {
+		if (c != '$') {
+			continue;
+		}
+
+		/* Skip space */
+		getchar();
+
+		if (getchar() != 'l') {
+			continue;
+		}
+		
+		/* Skip s\n */
+		getchar();
+		getchar();
+
+		
+	}
+}
 
 void solve(char* ans)
 {
-	int	sol;
+	int	size;
 
-	sol = 0;
+	size = 0;
 
-	sprintf(ans, "%d", sol);
+	sprintf(ans, "%d", size);
 }
